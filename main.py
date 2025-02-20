f324f2d45ca7bb944a4065c0a8b24130cc02be3f
Vlad <vladribak24@gmail.com>
2025-02-20 09:48:33 +0000

    second_commit

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('first_time')
+def removeElement(self, nums: List[int], val: int) -> int:
+    total = 0
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[total] = nums[i]
+            total += 1
+    return total
