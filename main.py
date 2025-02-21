7ab0ed290f6dddbafc2a80b6aefa612ba2902087
Vlad <vladribak24@gmail.com>
2025-02-21 10:49:21 +0000

    mac

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-def removeElement(self, nums: List[int], val: int) -> int:
-    total = 0
-    for i in range(len(nums)):
-        if nums[i] != val:
-            nums[total] = nums[i]
-            total += 1
-    return total
+print('first mac')
